1a19d9248734a18f4832ca9e222f681f2d6519ab
Bram Ceulemans <bramceulemans@me.com>
2020-12-04 17:01:38 +0100

    Refactor solutions for day 2

--- src/nativeMain/kotlin/day2/solutions.kt
+++ src/nativeMain/kotlin/day2/solutions.kt
@@ -4,7 +4,7 @@ fun part1() {
     var correct = 0
     data.forEach { entry ->
         val (amount, letter, password) = entry.split(" ")
-        val (min, max) = amount.split("-").map { it.toInt() }
+        val (min, max) = amount.split("-").map(String::toInt)
         val count = Regex(letter).findAll(password).count()
         val matches = count in min..max
 
@@ -15,15 +15,17 @@ fun part1() {
 
 fun part2() {
     val correct = data.map { it.split(" ") }
-        .map { entry ->
-            val (amount, letter, password) = entry
-            amount
-                .split("-")
-                .map { it.toInt().minus(1) }
-                .filter { i -> "${password[i]}" == letter }
-                .count()
-        }
+        .map(::checkPasswordPositions)
         .filter { it == 1 }
         .count()
     println("Correct: $correct")
+}
+
+fun checkPasswordPositions(entry: List<String>): Int {
+    val (amount, letter, password) = entry
+    return amount
+        .split("-")
+        .map { it.toInt().minus(1) }
+        .filter { i -> "${password[i]}" == letter }
+        .count()
 }
--- src/nativeMain/kotlin/main.kt
+++ src/nativeMain/kotlin/main.kt
@@ -9,15 +9,12 @@ fun runSolution(day: Int, part: Int, callback: () -> Unit) {
 
 @ExperimentalTime
 fun main() {
-//    runSolution(1, 1) { day1.part1() }
-//    runSolution(1, 2) { day1.part2() }
-//
-//    runSolution(2, 1) { day2.part1() }
-//    runSolution(2, 2) { day2.part2() }
-
-//    runSolution(3, 1) { day3.part1() }
-//    runSolution(3, 2) { day3.part2() }
-
+    runSolution(1, 1) { day1.part1() }
+    runSolution(1, 2) { day1.part2() }
+    runSolution(2, 1) { day2.part1() }
+    runSolution(2, 2) { day2.part2() }
+    runSolution(3, 1) { day3.part1() }
+    runSolution(3, 2) { day3.part2() }
     runSolution(4, 1) { day4.part1() }
     runSolution(4, 2) { day4.part2() }
 }
